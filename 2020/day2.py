918b968365b06344d68d6e4e4978c250ffbb44ad
Cas Obdam <17879738+Obdam@users.noreply.github.com>
2020-12-02 21:54:25 +0100

    Finished day 2 2020

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,9 +1,57 @@
+import re
+
+
 def importPasswords():
-    passwords = open('day2Input.txt', 'r')
+    passwords = []
+    with open("day2Input.txt", "r") as file:
+        for line in file:
+            passwords.append(line)
     return passwords
 
+
+# def checkPasswordPolicy(passwords):
+#     validPasswords = 0
+#     for password in passwords:
+#         splittedPassword = re.split(r'(-+|:+| )', password)
+#         minNum = int(splittedPassword[0])
+#         maxNum = int(splittedPassword[2])
+#         policyLetter = str(splittedPassword[4]).strip()
+#         distortedPass = str(splittedPassword[8]).strip()
+
+#         count = 0
+#         for letter in distortedPass:
+#             if letter == policyLetter:
+#                 count += 1
+
+#         if count >= minNum and count <= maxNum:
+#             validPasswords += 1
+
+#     return validPasswords
+
+
+def checkRealPasswordPolicy(passwords):
+    validPasswords = 0
+    for password in passwords:
+        splittedPassword = re.split(r'(-+|:+| )', password)
+        minNum = int(splittedPassword[0])
+        maxNum = int(splittedPassword[2])
+        policyLetter = str(splittedPassword[4]).strip()
+        distortedPass = str(splittedPassword[8]).strip()
+
+        if distortedPass[maxNum - 1] == policyLetter:
+            validPasswords += 1
+        elif distortedPass[minNum - 1] == policyLetter:
+            validPasswords += 1
+        
+        if (policyLetter == distortedPass[maxNum - 1] and policyLetter != distortedPass[minNum - 1]) or (policyLetter != distortedPass[maxNum - 1] and policyLetter == distortedPass[minNum - 1]):
+            validPasswords += 1
+
+    return validPasswords
+
 def main():
-    print(importPasswords())
+    passwordDump = importPasswords()
+    totalCorrect = checkRealPasswordPolicy(passwordDump)
+    print("The total correct passwords are: ", totalCorrect)
 
 if __name__ == "__main__":
     main()
