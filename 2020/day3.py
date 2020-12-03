97e4cb2d662234f61612683f766d38095681b39a
Cas Obdam <17879738+Obdam@users.noreply.github.com>
2020-12-03 22:02:15 +0100

    Finished day 3

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,17 +1,35 @@
 map = []
-row = 0
-column = 0
-trees = 0
+slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+totalTrees = 1
 
-with open('/mnt/d/Code/AdventOfCode2020/2020/day3input.txt', 'r') as file:
+with open('/mnt/c/Users/Cas Obdam/Documents/CodeProjects/AdventOfCode/2020/day3input.txt', 'r') as file:
     for line in file:
         map.append(str(line).strip())
 
+# Task 1
+row = 0
+column = 0
+trees = 0
 while row < len(map):
     if map[row][column % len(map[0])] == '#':
         trees += 1
 
     column += 3
     row += 1
-    
 print(trees)
+
+# Task 2
+for slope in slopes:
+    column = slope[0]
+    row = slope[1]
+    trees = 0
+    while row < len(map):
+        if map[row][column % len(map[0])] == '#':
+            trees += 1
+
+        column += slope[0]
+        row += slope[1]
+
+    totalTrees *= trees
+
+print(totalTrees)
