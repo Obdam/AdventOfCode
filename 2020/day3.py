0b52bdfa2fce2912d37f3c59eea48e5241209328
Cas Obdam <17879738+Obdam@users.noreply.github.com>
2020-12-03 12:43:56 +0100

    Current day 3

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,6 +1,11 @@
 map = []
-with open("day3Input.txt", "r") as file:
-    for line in file:
-        map.append(line)
+row = 0
+right = 3
+trees = 0
 
-print(map)
+with open("/mnt/d/Code/AdventOfCode2020/2020/day3input.txt", 'r') as file:
+    for line in file:
+        map.append(str(line).strip())
+
+while row + 1 < len(map):
+    
