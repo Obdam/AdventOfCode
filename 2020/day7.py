30eb24ef82f544148fe8085baca458603aca31f7
Cas Obdam <17879738+Obdam@users.noreply.github.com>
2020-12-15 16:32:46 +0100

    Finally finished day 7 task 1

--- 2020/day7.py
+++ 2020/day7.py
@@ -1,13 +1,15 @@
 import re
+shinycollection = []
 
 
-def parseData():
+def parsedata():
     rules = {}
     with open('/mnt/d/Code/AdventOfCode2020/2020/day7input.txt', 'r') as file:
         lines = file.read().split("\n")
         groups = [line.split("contain") for line in lines]
         for group in groups:
             group[0] = group[0].strip()
+            # print(group[0])
             group[1] = group[1].strip()
             ruleBags = group[1].split(',')
             rules[group[0].strip()] = {}
@@ -22,36 +24,23 @@ def parseData():
     return rules
 
 
-def checkBags(rules):
-    totalCount = 0
-    for rule in rules:
-        shinyFrequency = 0
-        ruleKeys = rules[rule].keys()
-        if 'shiny gold' in rule:
-            continue
-        for ruleKey in ruleKeys:
-            if 'shiny gold' in ruleKey:
-                totalCount += 1
-            else:
-                shinyFrequency += checkParentBag(ruleKey, rules)
-        if shinyFrequency >= 1:
-            totalCount += 1              
-        
-    print(totalCount)         
-
-def checkParentBag(ruleKey, rules):
-    shinyCount = 0
-    if 'bags' not in ruleKey:
-        ruleKey = ruleKey + 's'
-    keyList = rules[ruleKey].keys()
-    for key in keyList:
-        if 'shiny gold' in key:
-            shinyCount += 1
+def checkshiny(rule, data):
+    for child in rule:
+        if 'shiny gold' in child and child not in shinycollection:
+            return True
+        if 'bags' not in child:
+            child = child + 's'
+            if checkshiny(data[child], data):
+                return True
         else:
-            continue
+            if checkshiny(data[child], data):
+                return True
+    return False
 
-    return shinyCount
 
 if __name__ == "__main__":
-    data = parseData()
-    checkBags(data)
+    data = parsedata()
+    for rule in data:
+        if checkshiny(data[rule], data):
+            shinycollection.append(rule)
+    print(len(shinycollection))
