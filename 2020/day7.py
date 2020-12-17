72df4fcf9591ca70057351c3e5164a0629374c8b
Cas Obdam <17879738+Obdam@users.noreply.github.com>
2020-12-17 18:30:22 +0100

    Finally finished day 7

--- 2020/day7.py
+++ 2020/day7.py
@@ -9,7 +9,6 @@ def parsedata():
         groups = [line.split("contain") for line in lines]
         for group in groups:
             group[0] = group[0].strip()
-            # print(group[0])
             group[1] = group[1].strip()
             ruleBags = group[1].split(',')
             rules[group[0].strip()] = {}
@@ -20,7 +19,8 @@ def parsedata():
                     continue
                 else:
                     rule = re.split(r'(\d+) (.+?) bags?[,.]', rule)
-                    rules[group[0].strip()][rule[0][1:].strip()] = rule[0][0]
+                    rules[group[0].strip()][rule[0][1:].strip()
+                                            ] = int(rule[0][0])
     return rules
 
 
@@ -38,9 +38,29 @@ def checkshiny(rule, data):
     return False
 
 
+def countBagsInShiny(bag, data):
+    totalcount = 0
+    if bag == {}:
+        return 0
+
+    for child in bag:
+        directChildCount = bag[child]
+        if 'bags' not in child:
+            child = child + 's'
+        totalcount += directChildCount + \
+            (directChildCount * countBagsInShiny(data[child], data))
+
+    return totalcount
+
+
 if __name__ == "__main__":
     data = parsedata()
+
+    # Part 1
     for rule in data:
         if checkshiny(data[rule], data):
             shinycollection.append(rule)
     print(len(shinycollection))
+
+    # Part 2
+    print(countBagsInShiny(data['shiny gold bags'], data))
